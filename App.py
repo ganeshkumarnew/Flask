e319ae5b56eaa13dae81e4c14df090d4f5784c30
ganeshkumar93-digixhub <ganesh.kumar@digixhub.com>
2020-04-15 17:15:27 +0530

    flask with swagger

--- App.py
+++ App.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash,jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_restplus import Api, Resource
+from werkzeug.utils import cached_property
+from sqlalchemy import text
+
 
 app = Flask(__name__)
 app.secret_key = "Secret Key"
@@ -26,12 +30,28 @@ class Data(db.Model):
         self.phone = phone
 
 
+class Company(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    company_name = db.Column(db.String(100))
+    company_email = db.Column(db.String(100))
+    company_phone = db.Column(db.String(100))
 
+
+    def __init__(self, company_name, company_email, company_phone):
+
+        self.company_name = company_name
+        self.company_email = company_email
+        self.company_phone = company_phone
+
+
+api = Api()
+api.init_app(app)
+name_space = api.namespace('my_api', description='API Project')
 
 
 #This is the index route where we are going to
 #query on all our employee data
-@app.route('/')
+@app.route('/employees')
 def Index():
     all_data = Data.query.all()
     return render_template("index.html", employees = all_data)
@@ -87,6 +107,45 @@ def delete(id):
     return redirect(url_for('Index'))
 
 
+
+
+
+
+@name_space.route("/")
+class LogList(Resource):
+    def get(self):  # will be used to fetch all record from log later
+        sql = text('select company_name from company')
+        result = db.engine.execute(sql)
+        data = [row[0] for row in result]
+        response = jsonify(data)
+        response.status_code = 200 # or 400 or whatever
+        return response
+
+    def post(self):  # will be used to create a new log record
+        return {
+            "status": "Create new log"
+        }
+
+
+@name_space.route("/<int:id>")
+class LogDetail(Resource):
+    def get(self, id):  # will be used to see one particular log detail
+        sql = text('SELECT company_name FROM company WHERE id = \'{}\''.format(id))
+        result = db.engine.execute(sql)
+        data = [row[0] for row in result]
+        response = jsonify(data)
+        response.status_code = 200 # or 400 or whatever
+        return response
+
+    def put(self, id):  # will be used to update particular log
+        return {
+            "status": "Updated log with id " + str(id)
+        }
+
+    def delete(self, id):  # will be used to delete particular log
+        return {
+            "status": "Deleted log with id " + str(id)
+        }
 
 if __name__ == "__main__":
     app.run(debug=True)
--- README.md
+++ README.md
@@ -2,9 +2,17 @@
 
 pip install Flask
 pip install Flask-SQLAlchemy
+
+pip install flask-restplus
 after Integration Execute in Terminal Python
 
 >>>from App import db
 >>>db.create_all()
+
+$ export FLASK_APP=App.py
+Followed by
+$ export FLASK_ENVIRONMENT=development
+$ flask run
+pip install --upgrade werkzeug==0.16.1
 
 database automatically created and application will open in 127.0.0.1:5000/
